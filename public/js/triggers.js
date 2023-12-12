abe5ce209f7ea0d1a1aab8ed9aae0e46d660ebf8
Zeke-Goodman <120141215+Zeke-Goodman@users.noreply.github.com>
2023-12-12 16:24:57 -0700

    set up routes and trigger file for every page

--- public/js/triggers.js
+++ public/js/triggers.js
@@ -4,15 +4,20 @@ document.getElementById('about').addEventListener('click', function() {
 document.getElementById('palestine').addEventListener('click', function() {
     window.location.href = '/palestine';
 });
-document.getElementById('report').addEventListener('click', function() {
-    window.location.href = '/report';
-});
-document.getElementById('login').addEventListener('click', function() {
-    window.location.href = '/login';
-});
 document.getElementById('support').addEventListener('click', function() {
     window.location.href = '/support';
 });
-document.getElementById('index').addEventListener('click', function() {
+document.getElementById('home').addEventListener('click', function() {
+    console.log('Home link clicked');
     window.location.href = '/';
 });
+// document.getElementById('report').addEventListener('click', function() {
+//     window.location.href = '/report';
+// });
+// document.getElementById('login').addEventListener('click', function() {
+//     window.location.href = '/login';
+// });
+
+
+
+
