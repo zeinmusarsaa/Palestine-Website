7dcd9552cf9cb951fe8a4922849ee7f35e183eb6
Zeke-Goodman <120141215+Zeke-Goodman@users.noreply.github.com>
2023-12-12 14:14:14 -0700

    set up non href javascript functions and routes

---
+++ public/js/triggers.js
@@ -0,0 +1,18 @@
+document.getElementById('about').addEventListener('click', function() {
+    window.location.href = '/about';
+});
+document.getElementById('palestine').addEventListener('click', function() {
+    window.location.href = '/palestine';
+});
+document.getElementById('report').addEventListener('click', function() {
+    window.location.href = '/report';
+});
+document.getElementById('login').addEventListener('click', function() {
+    window.location.href = '/login';
+});
+document.getElementById('support').addEventListener('click', function() {
+    window.location.href = '/support';
+});
+document.getElementById('index').addEventListener('click', function() {
+    window.location.href = '/';
+});
